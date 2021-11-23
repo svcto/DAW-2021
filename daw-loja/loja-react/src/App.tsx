cfde9d7197f03f27d84d7982bd5946ed73033c55
Samuel Alves <samuewicto.alves@gmail.com>
2021-11-23 19:20:58 -0300

    ok

---
+++ daw-loja/loja-react/src/App.tsx
@@ -0,0 +1,30 @@
+function App() {
+  return ( 
+    <div>
+      <header className="navbar navbar-expand navbar-light">
+        <div className="container">
+          <h1 className="navbar-brand">DAW Loja</h1>
+ 
+          <nav>
+            <ul className="navbar-nav">
+              <li className="nav-item">
+                <a className="nav-link active" href="#">Home</a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link" href="#">Produtos</a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link" href="#">Sobre</a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link" href="#">Contato</a>
+              </li>
+            </ul>
+          </nav>
+        </div>
+      </header>
+    </div>
+  );
+}
+
+export default App;
